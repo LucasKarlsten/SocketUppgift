7390721bfa082e6d1dba1a2499f40a45685d7473
LucasKarlsten <lucas.karlsten@gmail.com>
2024-10-29 09:45:29 +0100

    Update server.py
    La till class chatroom

--- UDP/server.py
+++ UDP/server.py
@@ -1,11 +1,22 @@
 import socket
 
-HOST = '127.0.0.1'
-PORT = 12345
+
+class ChatRoom:
+    def __init__(self, host, port):
+        # Initiera en ny klass med host och port, sätter också upp en ny lista med cliens
+        self.host = '127.0.0.1'
+        self.port = 22322
+        self.clients = []
+        # Upprättar en socket som använder Ipv4 och som kommer skicka data över UDP
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        
+# HOST = '127.0.0.1'
+# PORT = 12345
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    sock.bind((HOST, PORT))
-    print(f'Server listening on adress; {HOST} : {PORT}')
+    sock.bind((ChatRoom.host, ChatRoom.port))
+    print(f'Server listening on adress; {ChatRoom.host} : {ChatRoom.port}')
 
     while True: 
         data, addr = sock.recvfrom(256)
